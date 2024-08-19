d8b655cfc0a6134e9437d913bcb2a9d1038a1ee8
taewonnn <thisis_tw94@naver.com>
2024-08-19 18:51:08 +0900

    feat: login 모달 - intercepting Routes 적용 - 주석

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -30,6 +30,10 @@ export default function Layout({ children, modal }: Props) {
  * app/(beforeLogin)/page.tsx 파일에서 로그인 버튼은  <Link href="i/flow/login" className={styles.login}> i/flow/login으로 연결되니
  * 해당 경로의 page.tsx가 보여지는게 맞으나, @modal 폴더에 intercepting Routes인 (.)i > flow > login > page.tsx가 가로채서 해당 파일을 보여주는 형태
  *
+ * 그럼 app/(beforeLogin)/i/flow/login/page.tsx 해당 경로의 파일은 필요가 없는가??
+ * => 필요하다. 언제? -> 브라우저 통해서 처음 들어올 때, 혹은 모달이 띄어진 상태에서 새로고침할 때 보여진다!
+ *
+ *
  * (.) - 같은 경로
  * (..) - 해당 경로에서 한 단계 위의 경로
  * (..)(..) - 해당 경로에서 두 단계 위의 경로
