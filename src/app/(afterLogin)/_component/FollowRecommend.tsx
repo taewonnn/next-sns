0aa003aa87d992ddfc4571c04726f761ac48163e
taewonnn <thisis_tw94@naver.com>
2024-08-20 16:58:48 +0900

    feat: 로그인 이후 메인 레이아웃 디자인

---
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import style from './followRecommend.module.css';
+import Image from 'next/image';
+
+export default function FollowRecommend() {
+  const onFollow = () => {};
+
+  const user = {
+    id: 'elonmusk',
+    nickname: 'Elon Musk',
+    image: '/yRsRRjGO.jpg',
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.userLogoSection}>
+        <div className={style.userLogo}>
+          <Image src={user.image} alt={user.id} width={40} height={40} />
+        </div>
+      </div>
+      <div className={style.userInfo}>
+        <div className={style.title}>{user.nickname}</div>
+        <div className={style.count}>@{user.id}</div>
+      </div>
+      <div className={style.followButtonSection}>
+        <button onClick={onFollow}>팔로우</button>
+      </div>
+    </div>
+  );
+}
